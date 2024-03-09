74b63b9df17a07f53c2c5118b01df99be2edb219
8845musign <hiroki.yokouchi@dr-ubie.com>
2024-03-09 23:28:37 +0900

    markdown output
    Signed-off-by: 8845musign <hiroki.yokouchi@dr-ubie.com>

--- schemes/99.tests.sql
+++ schemes/99.tests.sql
@@ -1,5 +1,5 @@
 REPLACE INTO articles (id, title, body)
 VALUES
-  (1, 'こんにちは', '#今日は\nいい天気'),
-  (2, 'Hello', 'hello world'),
+  (1, 'こんにちは', '# 今日は\nいい天気'),
+  (2, 'Hello', '# Hi, *Pluto*!'),
   (3, 'Bonjour', 'Bonjour le monde');
--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { Article } from "@/types";
 import { axios } from "@/lib/axios";
+import Markdown from "react-markdown";
 
 const Page = async ({ params }: { params: { id: string } }) => {
   const result = await axios.get<Article>(`/api/articles/${params.id}`);
@@ -7,7 +8,7 @@ const Page = async ({ params }: { params: { id: string } }) => {
   return (
     <main className="p-24">
       <h1>{result.data.title}</h1>
-      <p>{result.data.body}</p>
+      <Markdown>{result.data.body}</Markdown>
     </main>
   );
 };
